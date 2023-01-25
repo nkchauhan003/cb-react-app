26e88015c1269d3fd3cb18818b9294c0fd7e9db0
nkchauhan003 <nkchauhan003@gmail.com>
2023-01-25 15:41:05 +0530

    added new code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const h1Value = "Hello from the 'App' component.";
+  const items = [
+    {id: 1, name: "Apple"},
+    {id: 2, name: "Orange"},
+    {id: 3, name: "Guava"},
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="h1">{h1Value}</h1>
+      <p>Items ({items.length})</p>
+      <ul>
+        {items && items.map((item, index) => <li key={index}>{item.name}</li>)}
+      </ul>
+      {Math.random() + 10}
     </div>
   );
 }
-
 export default App;
